84f92c0d67317d76b9c0f3e4193f28079023a40e
Gustavo Pelissari <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-10-16 02:54:01 -0300

    Fit: Adiciona funções em Divisao

--- Calculadora_Java_POO/src/model/OperacoesBasicas/Divisao.java
+++ Calculadora_Java_POO/src/model/OperacoesBasicas/Divisao.java
@@ -1,8 +1,32 @@
 package model.OperacoesBasicas;
 
+import Ferramentas.Color;
 import model.Calculator;
 
 public class Divisao extends Calculator
 {
+    public Divisao(){}
 
+    public Divisao(Calculator calculator)
+    {
+        this.atualValue = calculator.getAtualValue();
+    }
+
+    @Override
+    public void operation(double value)
+    {
+        if(value == 0)
+        {
+            System.out.println(Color.VERMELHO+"Erro: Divisão por zero"+ Color.RESET);
+            return;
+        }
+        else if(!hasValue())
+        {
+            atualValue = value;
+        }
+        else
+        {
+            atualValue /= value;
+        }
+    }
 }
