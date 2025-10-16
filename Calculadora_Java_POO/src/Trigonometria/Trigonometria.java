a2732002140bef0f1361716813961599dec591c7
Gustavo Pelissari <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-10-16 11:35:51 -0300

    Fit: Adiciona as funções 'seno', 'cosseno' e 'tangente' em Trigonometria

--- Calculadora_Java_POO/src/Trigonometria/Trigonometria.java
+++ Calculadora_Java_POO/src/Trigonometria/Trigonometria.java
@@ -1,6 +1,46 @@
 package Trigonometria;
 
+import Ferramentas.Color;
+import Ferramentas.Delay;
+import Ferramentas.Reader;
+
+import java.util.Scanner;
+
 public class Trigonometria
 {
+    public static void seno() {
+        try {
+            System.out.print("Digite o ângulo (em graus): ");
+            double angule = Math.toRadians(Reader.rdDouble());
+            System.out.println("Seno: " + Math.sin(angule));
+            Delay.sleep(2000);
 
+        } catch (Exception e) {
+            System.out.println("Erro: entrada inválida!");
+        }
+    }
+
+    public static void cosseno() {
+        try {
+            System.out.print("Digite o ângulo (em graus): ");
+            double angule = Math.toRadians(Reader.rdDouble());
+            System.out.println("Cosseno: " + Math.cos(angule));
+            Delay.sleep(2000);
+
+        } catch (Exception e) {
+            System.out.println(Color.VERMELHO+"Erro: entrada inválida!"+Color.RESET);
+        }
+    }
+
+    public static void tangente() {
+        try {
+            System.out.print("Digite o ângulo (em graus): ");
+            double angule = Math.toRadians(Reader.rdDouble());
+            System.out.println("Tangente: " + Math.tan(angule));
+            Delay.sleep(2000);
+
+        } catch (Exception e) {
+            System.out.println(Color.VERMELHO+"Erro: entrada inválida!"+Color.RESET);
+        }
+    }
 }
