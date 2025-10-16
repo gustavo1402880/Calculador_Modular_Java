8fde57f93df00a461c99b4361f58715c9a681f67
Gustavo Pelissari <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-10-16 11:26:57 -0300

    Fit: Adiciona a função 'execute' em Module

--- Calculadora_Java_POO/src/Teoria/Module.java
+++ Calculadora_Java_POO/src/Teoria/Module.java
@@ -1,6 +1,24 @@
 package Teoria;
 
+import Ferramentas.Color;
+import Ferramentas.Delay;
+import Ferramentas.Reader;
+
 public class Module
 {
+    public static void execute()
+    {
+        try
+        {
+            System.out.print("Digite um número: ");
+            double value = Reader.rdDouble();
 
+            System.out.println("Abs("+value+") = "+Math.abs(value));
+            Delay.sleep(2000);
+        }
+        catch (Exception e)
+        {
+            System.out.println(Color.VERMELHO+"Erro: Entrada Inválida!"+Color.RESET);
+        }
+    }
 }
