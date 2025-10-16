e9fa85329ee772aaf29e3e12624abd36bb43bddf
Gustavo Pelissari <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-10-16 11:02:07 -0300

    Fit: Adiciona a função 'execute' em MDC

--- Calculadora_Java_POO/src/Teoria/MDC.java
+++ Calculadora_Java_POO/src/Teoria/MDC.java
@@ -1,6 +1,36 @@
 package Teoria;
 
+import Ferramentas.Color;
+import Ferramentas.Delay;
+import Ferramentas.Reader;
+
 public class MDC
 {
+    public static void execute()
+    {
+        try
+        {
+            System.out.print("Digite o primeiro número: ");
+            int valueA = Reader.rdInt();
+            System.out.print("Digite o segundo número: ");
+            int valueB = Reader.rdInt();
 
+            valueA = Math.abs(valueA);
+            valueB = Math.abs(valueB);
+
+            while(valueB != 0)
+            {
+                int remainder = valueA % valueB;
+                valueA = valueB;
+                valueB = remainder;
+            }
+
+            System.out.println("= "+ valueA);
+            Delay.sleep(1500);
+        }
+        catch (Exception e)
+        {
+            System.out.println(Color.VERMELHO+"Erro: Entrada Inválida!"+Color.RESET);
+        }
+    }
 }
