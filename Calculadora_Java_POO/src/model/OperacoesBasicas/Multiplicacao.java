137dac8fc9d657559e1455f094457108715b4302
Gustavo Pelissari <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-10-16 02:45:10 -0300

    Fit: Adiciona as funcoes em Multiplicacao

--- Calculadora_Java_POO/src/model/OperacoesBasicas/Multiplicacao.java
+++ Calculadora_Java_POO/src/model/OperacoesBasicas/Multiplicacao.java
@@ -4,5 +4,21 @@ import model.Calculator;
 
 public class Multiplicacao extends Calculator
 {
+    public Multiplicacao(){}
 
+    public Multiplicacao(Calculator calculator)
+    {
+        this.atualValue = calculator.getAtualValue();
+    }
+
+    @Override
+    public void operation(double value)
+    {
+        if(!hasValue())
+        {
+            atualValue = 0;
+        }
+        atualValue *= value;
+        showVisor();
+    }
 }
